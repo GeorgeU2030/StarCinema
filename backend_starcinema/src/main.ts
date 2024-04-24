cb34b58f387789b13b0f9e5710e638e06f59f087
GeorgeU2030 <georgeu2021@gmail.com>
2024-04-24 07:52:11 -0500

    deploy

--- backend_starcinema/src/main.ts
+++ backend_starcinema/src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000 || process.env.PORT);
+  await app.listen(3000) || process.env.PORT;
+  app.useGlobalPipes(new ValidationPipe())
+  app.enableCors({
+    methods: [ "GET", "POST", "PUT", "DELETE","OPTIONS", "PATCH"],
+    origin: process.env.FRONTEND_URL,
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+ });
 }
 bootstrap();
